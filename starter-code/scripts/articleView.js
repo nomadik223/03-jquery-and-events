77e79ff30d62f52ad6be354e07aee984f2fbabc2
Austin Rogers <contactALRogers@gmail.com>
2017-02-10 12:14:37 -0800

    Finished?

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -55,13 +55,15 @@ articleView.handleCategoryFilter = function() {
 };
 
 articleView.handleMainNav = function () {
-  $('.main-nav').on('click', '.tab', function() {
-    /* TODO:
+    /* DONE:
       1. Hide all of the .tab-content sections
       2. Fade in the single .tab-content section that is
         associated with the .tab element's data-content attribute.
     */
-  });
+    $('.main-nav').on('click', '.tab', function(e) {
+      $('.tab-content').hide();
+      $('#' + $(this).data('content')).fadeIn();
+    });
   $('.main-nav .tab:first').click();
 };
 
